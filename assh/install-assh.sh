1f43fb1cb555387d33499b58cb6ddc2773fdc51c
Heekang Park <park.heekang33@gmail.com>
2022-10-18 21:50:19 +0900

    build assh-1.2.3

--- assh/install-assh.sh
+++ assh/install-assh.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+# installer version : 1.0.0
+
 cd $HOME
 
 # download app
 rm -rf assh assh-*.tar.gz
 wget https://github.com/HeekangPark/utilities/raw/master/assh/assh-latest.tar.gz
 tar -zxf assh-latest.tar.gz
-mv assh-latest assh
 rm -rf assh-latest.tar.gz
 
 # install app (add to ~/scripts)
